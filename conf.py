a015cde25420bbf456ef5cf2c88c1689d05475d8
Jonathan Sick <jsick@lsst.org>
2021-11-15 16:19:23 -0500

    Use sphinx-diagrams
    This requires a newer version of Sphinx.

--- conf.py
+++ conf.py
@@ -1,25 +1,4 @@
-#!/usr/bin/env python
-#
-# Sphinx configuration file
-# see metadata.yaml in this repo to update document-specific metadata
-
-import os
-from documenteer.sphinxconfig.technoteconf import configure_technote
-
-# Ingest settings from metadata.yaml and use documenteer's configure_technote()
-# to build a Sphinx configuration that is injected into this script's global
-# namespace.
-metadata_path = os.path.join(os.path.dirname(__file__), 'metadata.yaml')
-with open(metadata_path, 'r') as f:
-    confs = configure_technote(f)
-g = globals()
-g.update(confs)
-
-# Add intersphinx inventories as needed
-# http://www.sphinx-doc.org/en/stable/ext/intersphinx.html
-# Example:
-#
-#     intersphinx_mapping['python'] = ('https://docs.python.org/3', None)
+from documenteer.conf.technote import *
 
 rst_epilog = """
 
@@ -27,3 +6,5 @@ rst_epilog = """
 
 .. _Jinja: https://jinja.palletsprojects.com/en/3.0.x/
 """
+
+extensions.append("sphinx_diagrams")
